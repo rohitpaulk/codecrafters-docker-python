f50cd9600c405247e57610cf4beaac55b744ac01
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-12-13 17:23:19 +0000

    pass stage 2

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -7,7 +8,8 @@ def main():
     args = sys.argv[4:]
 
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    sys.stdout.buffer.write(completed_process.stdout)
+    sys.stderr.buffer.write(completed_process.stderr)
 
 
 if __name__ == "__main__":
