22d5f62a79632b97bba82e984335ae69f7c8f71e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-01-11 17:39:02 +0000

    implement chroot

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 import os
 import subprocess
 import sys
+import tempfile
+import shutil
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    completed_process = subprocess.run([command, *args])
-    exit(completed_process.returncode)
+    with tempfile.TemporaryDirectory() as chroot_dir:
+        # Copy executable into chroot
+        os.makedirs(os.path.join(chroot_dir, os.path.dirname(command).strip("/")))
+        shutil.copy(command, os.path.join(chroot_dir, command.strip("/")))
+
+        os.chroot(chroot_dir)
+
+        completed_process = subprocess.run([command, *args])
+        exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
